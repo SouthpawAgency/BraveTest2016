a99e83c9cd0dfccaf681f85045c5dd2603ae71e0
Alex Webster <alex.webster@southpawagency.com>
2016-05-31 09:39:07 +0100

    -Transition stuff

--- start/js/app.js
+++ start/js/app.js
@@ -108,6 +108,74 @@ app.controller('Questionnaire', ['$scope', '$http', '$templateCache', function (
           }]
         }
 
+// transition
+
+jQuery(document).ready(function($){
+    //cache some jQuery objects
+    var modalTrigger = $('.cd-modal-trigger'),
+        transitionLayer = $('.cd-transition-layer'),
+        transitionBackground = transitionLayer.children(),
+        modalWindow = $('.cd-modal');
+
+    var frameProportion = 1.78, //png frame aspect ratio
+        frames = 25, //number of png frames
+        resize = false;
+
+    //set transitionBackground dimentions
+    setLayerDimensions();
+    $(window).on('resize', function(){
+        if( !resize ) {
+            resize = true;
+            (!window.requestAnimationFrame) ? setTimeout(setLayerDimensions, 3300) : window.requestAnimationFrame(setLayerDimensions);
+        }
+    });
+
+    //open modal window
+    modalTrigger.on('click', function(event){   
+        // event.preventDefault();
+        transitionLayer.addClass('visible closing');
+        var delay = ( $('.no-cssanimations').length > 0 ) ? 0 : 1600;
+        setTimeout(function(){
+            transitionLayer.removeClass('visible closing');
+            $('.cd-transition-layer').css('pointer-events','none');
+        }, delay);
+    });
+
+    //close modal window
+    modalWindow.on('click', '.modal-close', function(event){
+        // event.preventDefault();
+        transitionLayer.addClass('closing');
+        modalWindow.removeClass('visible');
+        transitionBackground.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+            transitionLayer.removeClass('closing opening visible');
+            transitionBackground.off('webkitAnimationEnd oanimationend msAnimationEnd animationend');
+        });
+    });
+
+    function setLayerDimensions() {
+        var windowWidth = $(window).width(),
+            windowHeight = $(window).height(),
+            layerHeight, layerWidth;
+
+        if( windowWidth/windowHeight > frameProportion ) {
+            layerWidth = windowWidth;
+            layerHeight = layerWidth/frameProportion;
+        } else {
+            layerHeight = windowHeight*1.2;
+            layerWidth = layerHeight*frameProportion;
+        }
+
+        transitionBackground.css({
+            'width': layerWidth*frames+'px',
+            'height': layerHeight+'px',
+        });
+
+        resize = false;
+    }
+});
+
+// transition end
+
     var theUser;
     var theUserRef;
     var backgrounds = ["Question1.jpg", "Question2.jpg", "Question3.jpg", "Question4.jpg", "Question5.jpg", "Question6.jpg", "Question7.jpg", "Question8.jpg", "Question9.jpg", "Question10.jpg", "Question10.jpg"];
