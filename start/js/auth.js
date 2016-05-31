666cba0c8fd17a2e1ae1da649e9a309ea8f8f58e
Liam Hayes <liam.hayes@southpawagency.com>
2016-05-31 09:44:04 +0100

    - Updated infographic scripts, share icons scripts

--- start/js/auth.js
+++ start/js/auth.js
@@ -227,11 +227,11 @@
 // Check the current user
       var user = rootRef.getAuth();
       var userRef;
-      var infoOneVal, infoOneValAll, infoOneOption, infoOneOptionAll,
-          infoTwoVal, infoTwoValAll, infoTwoOption, infoTwoOptionAll,
-          infoThreeVal, infoThreeValAll, infoThreeOption, infoThreeOptionAll,
-          infoFourVal, infoFourValAll, infoFourOption, infoFourOptionAll,
-          infoFiveVal, infoFiveValAll, infoFiveOption, infoFiveOptionAll;
+      var infoOneVal, infoOneOption,
+          infoTwoVal, infoTwoOption,
+          infoThreeVal, infoThreeOption,
+          infoFourVal, infoFourOption,
+          infoFiveVal, infoFiveOption, infoFiveOptionAlt;
 
       // If no current user send to register page
       if (!user) {
@@ -246,133 +246,71 @@
           if (!user) {
               return;
           }
-          var braveType = user.braveCategory;
-          var infoRef = dataRef.child(braveType);
 
           dataRef.once('value', function (snap) {
               //main
               var allInfo = snap.val();
-              var profileInfo = allInfo[braveType];
+
               //Q1
-              var profileQ1 = profileInfo.profileQ1;
-              var snowboarding = profileQ1.snowboarding;
-              var snowboardingAll = allInfo.Adventurer.profileQ1.snowboarding + allInfo.Leader.profileQ1.snowboarding + allInfo.Protector.profileQ1.snowboarding + allInfo.Scientist.profileQ1.snowboarding;
-              var skiing = profileQ1.skiing;
-              var skiingAll = allInfo.Adventurer.profileQ1.skiing + allInfo.Leader.profileQ1.skiing + allInfo.Protector.profileQ1.skiing + allInfo.Scientist.profileQ1.skiing;
-              var Q1total = profileQ1.total;
-              var Q1totalAll = allInfo.Adventurer.profileQ1.total + allInfo.Leader.profileQ1.total + allInfo.Protector.profileQ1.total + allInfo.Scientist.profileQ1.total;
-              if (snowboarding >= skiing) {
-                infoOneVal = (snowboarding/Q1total)*100;
-                infoOneValAll = (snowboardingAll/Q1totalAll)*100;
-                infoOneOption = "snowboarding";
-                infoOneOptionAll = "snowboarding";
+              var Q1 = allInfo.Q1;
+              if (Q1.optionLeft.braveScore >= Q1.optionRight.braveScore) {
+                infoOneOption = "Jazz";
               } else {
-                infoOneVal = (skiing/Q1total)*100;
-                infoOneValAll = (skiingAll/Q1totalAll)*100;
-                infoOneOption = "skiing";
-                infoOneOptionAll = "skiing";
+                infoOneOption = "Hip Hop";
               }
               form.find('#infoOneOption').html(infoOneOption);
-              form.find('#infoOneOptionAll').html(infoOneOptionAll);
-              form.find('#infoOneVal').html(Math.round(infoOneVal));
-              form.find('#infoOneValAll').html(Math.round(infoOneValAll));
               //Q2
-              var profileQ2 = profileInfo.profileQ2;
-              var matureCheddar = profileQ2["mature cheddar"];
-              var matureCheddarAll = allInfo.Adventurer.profileQ2["mature cheddar"] + allInfo.Leader.profileQ2["mature cheddar"] + allInfo.Protector.profileQ2["mature cheddar"] + allInfo.Scientist.profileQ2["mature cheddar"];
-              var blueCheese = profileQ2["blue cheese"];
-              var blueCheeseAll = allInfo.Adventurer.profileQ2["blue cheese"] + allInfo.Leader.profileQ2["blue cheese"] + allInfo.Protector.profileQ2["blue cheese"] + allInfo.Scientist.profileQ2["blue cheese"];
-              var Q2total = profileQ2.total;
-              var Q2totalAll = allInfo.Adventurer.profileQ2.total + allInfo.Leader.profileQ2.total + allInfo.Protector.profileQ2.total + allInfo.Scientist.profileQ2.total;
-              if (matureCheddar >= blueCheese) {
-                infoTwoVal = (matureCheddar/Q2total)*100;
-                infoTwoValAll = (matureCheddarAll/Q2totalAll)*100;
-                infoTwoOption = "mature cheddar";
-                infoTwoOptionAll = "mature cheddar";
+              var Q2 = allInfo.Q2;
+              if (Q2.optionLeft.braveScore >= Q2.optionRight.braveScore) {
+                infoTwoOption = "lights on";
               } else {
-                infoTwoVal = (blueCheese/Q2total)*100;
-                infoTwoValAll = (blueCheeseAll/Q2totalAll)*100;
-                infoTwoOption = "blue cheese";
-                infoTwoOptionAll = "blue cheese";
-
+                infoTwoOption = "lights off";
               }
               form.find('#infoTwoOption').html(infoTwoOption);
-              form.find('#infoTwoOptionAll').html(infoTwoOptionAll);
-              form.find('#infoTwoVal').html(Math.round(infoTwoVal));
-              form.find('#infoTwoValAll').html(Math.round(infoTwoValAll));
               //Q3
-              var profileQ3 = profileInfo.profileQ3;
-              var electronic = profileQ3["electronic music"];
-              var rock = profileQ3["rock music"];
-              var Q3total = profileQ3.total;
-              if (electronic >= rock) {
-                infoThreeVal = Q3total/electronic;
-                infoThreeOption = "electronic music";
+              var Q3 = allInfo.Q3;
+              if (Q3.optionLeft.braveScore >= Q3.optionRight.braveScore) {
+                infoThreeOption = "Beer";
               } else {
-                infoThreeVal = Q3total/rock;
-                infoThreeOption = "rock music";
+                infoThreeOption = "Spirit";
               }
               form.find('#infoThreeOption').html(infoThreeOption);
-              form.find('#infoThreeVal').html(Math.round(infoThreeVal));
               //Q4
-              var profileQ4 = profileInfo.profileQ4;
-              var tea = profileQ4["tea"];
-              var teaAll = allInfo.Adventurer.profileQ4.tea + allInfo.Leader.profileQ4.tea + allInfo.Protector.profileQ4.tea + allInfo.Scientist.profileQ4.tea;
-              var coffee = profileQ4["coffee"];
-              var coffeeAll = allInfo.Adventurer.profileQ4.coffee + allInfo.Leader.profileQ4.coffee + allInfo.Protector.profileQ4.coffee + allInfo.Scientist.profileQ4.coffee;
-              var Q4total = profileQ4.total;
-              var Q4totalAll = allInfo.Adventurer.profileQ4.total + allInfo.Leader.profileQ4.total + allInfo.Protector.profileQ4.total + allInfo.Scientist.profileQ4.total;
-              if (tea >= coffee) {
-                infoFourVal = (tea/Q4total)*100;
-                infoFourValAll = (teaAll/Q4totalAll)*100;
-                infoFourOption = "tea";
-                infoFourOptionAll = "tea";
+              var Q4 = allInfo.Q4;
+              if (Q4.optionLeft.braveScore >= Q4.optionRight.braveScore) {
+                infoFourOption = "Game of Thrones";
               } else {
-                infoFourVal = (coffee/Q4total)*100;
-                infoFourValAll = (coffeeAll/Q4totalAll)*100;
-                infoFourOption = "coffee";
-                infoFourOptionAll = "coffee";
+                infoFourOption = "Breaking Bad";
               }
               form.find('#infoFourOption').html(infoFourOption);
-              form.find('#infoFourOptionAll').html(infoFourOptionAll);
-              form.find('#infoFourVal').html(Math.round(infoFourVal));
-              form.find('#infoFourValAll').html(Math.round(infoFourValAll));
-
-              //Q5
-              var profileQ5 = profileInfo.profileQ5;
-              var beach = profileQ5["beach holiday"];
-              var beachAll = allInfo.Adventurer.profileQ5["beach holiday"] + allInfo.Leader.profileQ5["beach holiday"] + allInfo.Protector.profileQ5["beach holiday"] + allInfo.Scientist.profileQ5["beach holiday"];
-              var city = profileQ5["city break"];
-              var cityAll = allInfo.Adventurer.profileQ5["city break"] + allInfo.Leader.profileQ5["city break"] + allInfo.Protector.profileQ5["city break"] + allInfo.Scientist.profileQ5["city break"];
-              var Q5total = profileQ5.total;
-              var Q5totalAll = allInfo.Adventurer.profileQ5.total + allInfo.Leader.profileQ5.total + allInfo.Protector.profileQ5.total + allInfo.Scientist.profileQ5.total;
-              if (beach >= city) {
-                infoFiveVal = (beach/Q5total)*100;
-                infoFiveValAll = (beachAll/Q5totalAll)*100;
-                infoFiveOption = "beach holidays";
-                infoFiveOptionAll = "beach holidays";
+              //Q4
+              var Q5 = allInfo.Q5;
+              if (Q5.optionLeft.braveScore >= Q5.optionRight.braveScore) {
+                infoFiveOption = "Nik Naks";
+                infoFiveOptionAlt = "Quavers";
               } else {
-                infoFiveVal = (city/Q5total)*100;
-                infoFiveValAll = (cityAll/Q5totalAll)*100;
-                infoFiveOption = "city breaks";
-                infoFiveOptionAll = "city breaks";
+                infoFiveOption = "Quavers";
+                infoFiveOptionAlt = "Nik Naks";
               }
               form.find('#infoFiveOption').html(infoFiveOption);
-              form.find('#infoFiveOptionAll').html(infoFiveOptionAll);
-              form.find('#infoFiveVal').html(Math.round(infoFiveVal));
-              form.find('#infoFiveValAll').html(Math.round(infoFiveValAll));
+              form.find('#infoFiveOptionAlt').html(infoFiveOptionAlt);
 
+              //overall average
+              var overallTotalScore = (Q1.optionLeft.braveScore * Q1.optionLeft.n) +
+                                      (Q1.optionRight.braveScore * Q1.optionRight.n) +
+                                      (Q2.optionLeft.braveScore * Q2.optionLeft.n) +
+                                      (Q2.optionRight.braveScore * Q2.optionRight.n) +
+                                      (Q3.optionLeft.braveScore * Q3.optionLeft.n) +
+                                      (Q3.optionRight.braveScore * Q3.optionRight.n) +
+                                      (Q4.optionLeft.braveScore * Q4.optionLeft.n) +
+                                      (Q4.optionRight.braveScore * Q4.optionRight.n) +
+                                      (Q5.optionLeft.braveScore * Q5.optionLeft.n) +
+                                      (Q5.optionRight.braveScore * Q5.optionRight.n);
+              var overallTotalN = Q1.optionLeft.n + Q1.optionRight.n + Q2.optionLeft.n + Q2.optionRight.n + Q3.optionLeft.n + Q3.optionRight.n + Q4.optionLeft.n + Q4.optionRight.n + Q5.optionLeft.n + Q5.optionRight.n;
+              var overallAvg = overallTotalScore/overallTotalN;
+              form.find('#infoOverallAvg').html(Math.round(overallAvg));
           });
-
-
-
-          // set the fields
-
-          form.find('#braveCategory').html(braveType);
       });
-
-
-
     };
 
     controllers.register = function (form) {
